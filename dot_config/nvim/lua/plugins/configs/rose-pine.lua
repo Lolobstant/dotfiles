e938155ee7a2a8e63076daf09831aa85bd4d9463
lolobstant <lolokun@gmail.com>
2024-06-12 08:10:56 +0200

    Update .config/nvim/lua/plugins/configs/rose-pine.lua Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/plugins/configs/rose-pine.lua
+++ dot_config/nvim/lua/plugins/configs/rose-pine.lua
@@ -1,17 +1,17 @@
 return {
-  -- disable_italics = false,
-  dark_variant = 'main',
-  dim_inactive_windows = true,
-  styles = {
-    -- transparency = true
-    italic = true,
-  },
-  -- highlight_groups = {
-  --   Comment = { italic = true },
-  --   keywords = { italic = true }
-  -- }
-  -- bold_vert_split = true,
-  -- groups = {
-  --   background = "#191724"
-  -- }
+	-- disable_italics = false,
+	dark_variant = "main",
+	dim_inactive_windows = true,
+	styles = {
+		-- transparency = true
+		italic = true,
+	},
+	highlight_groups = {
+		-- Comment = { italic = true },
+		-- keywords = { italic = true }
+	},
+	-- bold_vert_split = true,
+	-- groups = {
+	--   background = "#191724"
+	-- }
 }
