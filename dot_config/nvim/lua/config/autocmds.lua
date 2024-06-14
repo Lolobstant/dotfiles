c4ea403c89d988c4f06149299fac47bc8c805d1a
lolobstant <lolokun@gmail.com>
2024-06-14 11:08:07 +0200

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -8,30 +8,40 @@ augroup("AutoCommands", { clear = true })
 
 -- HIGHLIGHT YANKING TEXT
 autocmd("TextYankPost", {
-	group = "AutoCommands",
-	desc = "Hightlight selection on yank",
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 120 })
-	end,
+  group = "AutoCommands",
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 120 })
+  end,
 })
 
 if is_available("mini.files") then
-	local mini_files = require("mini.files")
+  local mini_files = require("mini.files")
 
-	local files_set_cwd = function()
-		local cur_entry_path = mini_files.get_fs_entry().path
-		local cur_directory = vim.fs.dirname(cur_entry_path)
-		vim.fn.chdir(cur_directory)
-		print("path changed to " .. cur_directory)
-	end
+  local files_set_cwd = function()
+    local cur_entry_path = mini_files.get_fs_entry().path
+    local cur_directory = vim.fs.dirname(cur_entry_path)
+    vim.fn.chdir(cur_directory)
+    print("path changed to " .. cur_directory)
+  end
 
-	autocmd("User", {
-		group = "AutoCommands",
-		pattern = "MiniFilesBufferCreate",
-		callback = function(args)
-			print("mini file set path")
-			vim.keymap.set("n", ".", files_set_cwd, { buffer = args.data.buf_id, desc = "Set current directory" })
-		end,
-	})
+  autocmd("User", {
+    group = "AutoCommands",
+    pattern = "MiniFilesBufferCreate",
+    callback = function(args)
+      print("mini file set path")
+      vim.keymap.set("n", ".", files_set_cwd, { buffer = args.data.buf_id, desc = "Set current directory" })
+    end,
+  })
 end
+
+-- autocmd({ "WinEnter", "BufEnter", "InsertLeave" }, {
+--  group = "AutoCommands",
+--  pattern = "*",
+--  desc = "Highlight trailing space",
+--  callback = function()
+--    vim.notify("highlight trailing")
+--    vim.fn.matchadd("MiniTrailspace", [[\s\+$]])
+--  end,
+-- })
