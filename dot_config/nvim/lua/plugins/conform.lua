1c463d1de587f8edbd0c6e512728bc2b29d7f116
lolobstant <lolokun@gmail.com>
2026-04-11 12:14:15 +0200

    Update .config/nvim/lua/plugins/conform.lua

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,46 +1,45 @@
-return { -- Autoformat
+return {
   "stevearc/conform.nvim",
-  -- lazy = false,
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
   opts = {
     notify_on_error = false,
+
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return
+      end
       return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        timeout_ms = 2000,
+        lsp_fallback = true,
       }
     end,
+
     default_format_opts = {
       stop_after_first = true,
     },
+
     formatters_by_ft = {
       lua = { "stylua" },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      javascript = { "prettierd", "prettier", "biome", "standardjs" },
+      javascript = { "prettierd", "prettier", "biome" },
+      typescript = { "prettierd", "prettier", "biome" },
+      javascriptreact = { "prettierd", "prettier", "biome" },
+      typescriptreact = { "prettierd", "prettier", "biome" },
+      html = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+      scss = { "prettierd", "prettier" },
+      json = { "prettierd", "prettier", "biome" },
+      jsonc = { "prettierd", "prettier", "biome" },
+      yaml = { "prettierd", "prettier" },
+      markdown = { "prettierd", "prettier" },
+      graphql = { "prettierd", "prettier" },
     },
+
     formatters = {
       stylua = {
         env = {
           indent_type = "Spaces",
-        },
-      },
-      biome = {
-        env = {
-          javascript = {
-            formatter = {
-              indentStyle = "space",
-              quoteStyle = "single",
-            },
-          },
         },
       },
     },
