8b05b9edb9c901cf0883b2aaaf2f1341f091a37a
lolobstant <lolokun@gmail.com>
2026-04-12 20:13:12 +0200

    Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/git.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -126,12 +126,6 @@ end
 local function apply_hl_overrides()
   -- vim.cmd.colorscheme("patana")
   vim.api.nvim_set_hl(0, "Constant", { fg = "#ff5858" })
-  -- Neogit lit les couleurs du colorscheme dans make_palette().
-  -- On force son re-init APRÈS que tous les ColorScheme listeners ont tourné,
-  -- garantissant que patana a bien tout appliqué avant que neogit lise les couleurs.
-  if package.loaded["neogit"] then
-    require("neogit.lib.hl").setup(require("neogit.config").values)
-  end
 end
 
 vim.api.nvim_create_autocmd("ColorScheme", {
